441f1295772fc61ff6d597b4e1169a35bfca70a2
aungkbhonehant <aungkhant.gsc@gmail.com>
2023-01-04 20:56:06 +0630

    Middleware

--- server.js
+++ server.js
@@ -1,7 +1,40 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const port = process.env.PORT || 3000;
+
+
+//custom middleware
+
+app.use(logger)
+
+const Wl = [
+    'https://www.google.com',
+    'http://localhost:3000',
+    'http://127.0.0.1:8000'
+]
+const corsOptions = {
+    origin:(origin, callback) => {
+        if(Wl.indexOf(origin) !== -1 || !origin){
+            callback(null, true)
+        }else{
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
+
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, './public')));
 
 // route
 app.get('^/$|/index(.html)?', (req, res) => {
@@ -50,5 +83,7 @@ app.get('/*', (req, res) => {
     //res.status(404).send('not found');
     res.sendFile(path.join(__dirname, 'views', '404.html'))
 })
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
